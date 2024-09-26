a1ab395eb5551400b4ac7ccece42bdb5f50bec60
Alex Kurylovich <rosenrot333777@gmail.com>
2024-09-27 00:43:57 +0200

    buffers and lazygit

--- dot_config/wip_exact_nvim/lua/config/keymaps.lua
+++ dot_config/wip_exact_nvim/lua/config/keymaps.lua
@@ -1,12 +1,13 @@
 local map = vim.keymap.set
 
+local telescope = require('telescope.builtin')
+local buffer = require('utils.buffer')
+local lazygit = require('utils.lazygit')
+
 -- Telescope
-local builtin = require('telescope.builtin')
-
-
-map('n', '<leader> ', builtin.find_files, { desc = 'Telescope find files' })
-map('n', '<leader>/', builtin.live_grep, { desc = 'Telescope live grep' })
-map('n', '<leader>,', builtin.buffers, { desc = 'Telescope buffers' })
+map('n', '<leader> ', telescope.find_files, { desc = 'Telescope find files' })
+map('n', '<leader>/', telescope.live_grep, { desc = 'Telescope live grep' })
+map('n', '<leader>,', telescope.buffers, { desc = 'Telescope buffers' })
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
@@ -21,15 +22,16 @@ map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-Up>", "<cmd>resize +10<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -10<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -10<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +10<cr>", { desc = "Increase Window Width" })
 
 -- buffers
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Next Buffer" })
+map("n", "<leader>bd", buffer.remove, { desc = "Delete Buffer" })
+map("n", "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", { desc = "Delete Other Buffers" })
 
 -- Clear search with <esc>
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and Clear hlsearch" })
@@ -45,8 +47,6 @@ map("v", ">", ">gv")
 map("n", "<leader>w", "<c-w>", { desc = "Windows", remap = true })
 map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
 
--- tabs
-map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
-map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+-- Lazygit
+map("n", "<leader>gb", lazygit.blame_line, { desc = "Git Blame Line" })
+
