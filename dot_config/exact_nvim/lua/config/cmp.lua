ae0580c8a3090ce9c4adcfa3d685aa15a8ba6165
Alex Kurylovich <rosenrot333777@gmail.com>
2024-10-08 00:16:40 +0200

    more cmp

--- dot_config/exact_nvim/lua/config/cmp.lua
+++ dot_config/exact_nvim/lua/config/cmp.lua
@@ -10,6 +10,28 @@ require("copilot_cmp").setup()
 -- CMP
 local cmp = require("cmp")
 
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" }
+  }
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" }
+  },
+  {
+    {
+      name = "cmdline",
+      option = {
+        ignore_cmds = { "Man", "!" }
+      }
+    }
+  })
+})
+
 cmp.setup({
   sources = {
     { name = "copilot" },
@@ -22,6 +44,8 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
   }),
 })
 
