56c5e82900204deebf3bd7f3e75187b55e9165d0
Alex Kurylovich <rosenrot333777@gmail.com>
2024-12-24 00:13:05 +0100

    code actions

--- dot_config/exact_nvim/exact_lua/exact_config/cmp.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/cmp.lua
@@ -150,4 +150,5 @@ vim.diagnostic.config({
 vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show diagnostics in a float" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { desc = "Code Actions" })
 
