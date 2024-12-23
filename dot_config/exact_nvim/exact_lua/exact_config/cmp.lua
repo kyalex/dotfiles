deb482da1c2ea0132025c4b3352e3dc89639ca15
Alex Kurylovich <rosenrot333777@gmail.com>
2024-12-24 00:00:57 +0100

    expand diagnostics

--- dot_config/exact_nvim/exact_lua/exact_config/cmp.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/cmp.lua
@@ -128,3 +128,26 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.ruby_lsp.setup { capabilities = capabilities }
 lspconfig.ts_ls.setup { capabilities = capabilities }
+
+-- Diagnostic
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●", -- Change prefix for diagnostic messages
+    spacing = 4,  -- Add spacing between diagnostic and text
+  },
+  signs = true,  -- Show signs in the sign column
+  underline = true, -- Underline the diagnostic text
+  severity_sort = true, -- Sort diagnostics by severity
+  float = {
+    border = "rounded", -- Rounded border for floating windows
+    source = true,  -- Show source of diagnostic
+    header = "Diagnostics", -- Header for the float window
+  },
+  update_in_insert = false, -- Disable diagnostics updates in insert mode
+})
+
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show diagnostics in a float" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+
