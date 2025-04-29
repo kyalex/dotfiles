1824326358740a5375d12ec97fe5f2a2a70e74a3
Alex Kurylovich <rosenrot333777@gmail.com>
2025-04-29 15:26:45 +0200

    no space in ruby array

--- dot_config/exact_nvim/exact_lua/exact_plugins/teesj.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/teesj.lua
@@ -4,8 +4,20 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
   config = function()
     local tsj = require("treesj")
+    local lang_utils = require('treesj.langs.utils')
 
-    tsj.setup({ use_default_keymaps = false })
+    tsj.setup({
+      use_default_keymaps = false,
+      langs = {
+        ruby = {
+          array = lang_utils.set_preset_for_list({
+            join = {
+              space_in_brackets = false,
+            }
+          })
+        }
+      }
+    })
 
     vim.keymap.set("n", "<leader>m", tsj.toggle)
   end,
