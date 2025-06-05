bb223adb48b62208c750b681b33425eb46b5e4a5
Alex Kurylovich <rosenrot333777@gmail.com>
2025-06-05 15:59:28 +0200

    oil

--- dot_config/exact_nvim/exact_lua/exact_plugins/neo-tree.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/neo-tree.lua
@@ -43,5 +43,13 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
+      },
+    },
   }
 }
