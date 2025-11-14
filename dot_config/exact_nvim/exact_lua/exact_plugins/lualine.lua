4c90ad38cd09ab9e86f74abfa1024489be700577
Alex Kurylovich <rosenrot333777@gmail.com>
2025-11-14 17:52:03 +0100

    show full file path in lua line

--- dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/lualine.lua
@@ -4,6 +4,12 @@ return {
   opts = {
     options = {
       globalstatus = true,
-    }
+    },
+    sections = {
+      lualine_c = { { 'filename', path = 1 } },
+    },
+    inactive_sections = {
+      lualine_c = { { 'filename', path = 1 } },
+    },
   }
 }
