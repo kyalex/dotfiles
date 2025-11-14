3fefdba56020a85556dd6d56aae7695dfdad5ad8
Alex Kurylovich <rosenrot333777@gmail.com>
2025-11-14 16:29:47 +0100

    some gh

--- dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -6,13 +6,16 @@ return {
     picker = { enabled = true },
   },
   keys = {
-    -- Top Pickers & Explorer
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
-    -- find
-    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+     -- git
+    { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+    { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+    { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+    -- gh
+    { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
   }
 }
