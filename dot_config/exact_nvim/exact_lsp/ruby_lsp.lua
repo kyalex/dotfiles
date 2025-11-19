e58c7d13d01404fe5063b278f56a4d03d3da0b30
Alex Kurylovich <rosenrot333777@gmail.com>
2025-11-19 17:17:01 +0100

    ruby lsp hide migration error

--- dot_config/exact_nvim/exact_lsp/ruby_lsp.lua
+++ dot_config/exact_nvim/exact_lsp/ruby_lsp.lua
@@ -2,15 +2,12 @@ return {
   cmd = {
     "mise", "exec", "--", "ruby-lsp"
   },
-  filetypes = { "ruby", "slim" },
-  root_markers = { "Gemfile", ".git", "." },
-  settings = {
-    ruby_lsp = {
-      enabledFeatures = {
-        "formatting", -- Enable LSP formatting
-        "diagnostics", -- Enable diagnostics
-        "codeActions", -- Enable code actions
+  filetypes = { "ruby", "slim" },  init_options = {
+    addonSettings = {
+      ["Ruby LSP Rails"] = {
+        enablePendingMigrationsPrompt = false,
       },
     },
   },
+  root_markers = { "Gemfile", ".git", "." },
 }
