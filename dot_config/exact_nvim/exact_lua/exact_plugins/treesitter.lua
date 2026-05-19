ea7b9439b8571e0d337cce690c71a274cb360425
Alex Kurylovich <rosenrot333777@gmail.com>
2026-05-19 13:10:17 +0200

    update treesitter

--- dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/treesitter.lua
@@ -1,54 +1,36 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = {
-        "ruby",
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline",
-        "javascript",
-        "typescript",
-        "tsx",
-        "sql",
-        "yaml",
-        "html"
-      },
+    require("nvim-treesitter").install({
+      "ruby",
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "javascript",
+      "typescript",
+      "tsx",
+      "sql",
+      "yaml",
+      "html",
+    })
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      highlight = {
-        enable = true,
-
-        disable = function(_, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-      endwise = {
-        enable = true,
-      },
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        local bufnr = args.buf
+        local max_filesize = 100 * 1024
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+        if ok and stats and stats.size > max_filesize then
+          return
+        end
+        pcall(vim.treesitter.start, bufnr)
+      end,
     })
 
     require("nvim-ts-autotag").setup({
@@ -63,9 +45,6 @@ return {
     })
   end,
   dependencies = {
-    { "RRethy/nvim-treesitter-endwise" },
     { "windwp/nvim-ts-autotag" },
-    { "nvim-treesitter/nvim-treesitter-textobjects" },
-  }
+  },
 }
-
